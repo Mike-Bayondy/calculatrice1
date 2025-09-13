59d57f84c35b28e74de9300cf12ace546490285d
Mike-Bayondy <bayondyedouard@gmail.com>
2025-09-13 11:01:36 -0400

    mike

---
+++ src/calculatriceapp/MainApp.java
@@ -0,0 +1,9 @@
+package calculatriceapp;
+
+import javax.swing.SwingUtilities;
+
+public class MainApp {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new CalculatriceUI());
+    }
+}
